f900e1a78340ba028ecd5adc6a288860dc2ccf6d
adrw <adrw@192-168-1-9.tpgi.com.au>
2021-01-28 23:55:16 +1100

    improved the Get-Okta function parameters

--- PSOkta/PSOkta.psm1
+++ PSOkta/PSOkta.psm1
@@ -4,61 +4,91 @@
 # Import Config
 $config = Import-PowerShellDataFile -Path ./Config.psd1
 
-<#
+
+function Get-Okta {
+  <#
  .Synopsis
-  Request Okta API.
-
- .Description
-  This function works with the Okta API to "Get" information based on the query/parameters you provide.
-
- .Parameter Version
-  The API versoin, defaults to V1.
-
- .Parameter Endpoint
-  The API endpoint you are querying, such as users, groups, applications.
+  Make requests to Okta API.
 
  .Example
    # Query the user profile.
    RequestOkta -Version "v1" -Endpoint "users" -QueryString "username@hillsong.com" 
 
  .Example
-   # Get Okta ACTIVE users.
-   RequestOkta -Version "v1" -Endpoint "users" -QueryString "username@hillsong.com"
+   # Get Okta ACTIVE users. Note: The -Version paramater defaults to "v1" and can be ommitted.
+   RequestOkta -Endpoint "users" -QueryString "status = 'ACTIVE'"
 
 #>
-function RequestOkta {
-    [CmdletBinding()]
-    param (
-        [Parameter()]
-        [String]
-        $Version = "v1",
-        [Parameter(Mandatory=$true)]
-        [String]
-        $Endpoint,
-        [Parameter()]
-        [String]
-        $QueryString
-    )
+  [CmdletBinding()]
+  param (
+    [Parameter()]
+    [String]
+    $Version = "v1",
+    [Parameter(Mandatory = $true)]
+    [String]
+    $Endpoint,
+    [Parameter()]
+    [String]
+    $Query,
+    [Parameter()]
+    [Switch]
+    $Q,
+    [Parameter()]
+    [Switch]
+    $Filter,
+    [Parameter()]
+    [Switch]
+    $Search
+  )
+
+  begin {
+    # Handle the parameters
+    $QueryString = $Query
+    if ($Q) { $QueryString = -join ("?q=", $Query) }
+    if ($Filter) { $QueryString = -join ("?filter=", $Query) }
+    if ($Search) { $QueryString = -join ("?search=", $Query) }
+
 
     # Setup Auth Header
-$headers = @{
-    Authorization = "SSWS {0}" -f $config.token
-    Accept = "application/json"
-    "Content-Type" = "application/json"
-}
+    $headers = @{
+      Authorization  = "SSWS {0}" -f $config.token
+      Accept         = "application/json"
+      "Content-Type" = "application/json"
+    }
 
-# API Resource Endpoint
-$uri = "https://hillsong-admin.okta.com/api/{0}/{1}{2}" -f $Version, $Endpoint, $QueryString #?filter=status eq "ACTIVE"'
+    # API Resource Endpoint
+    $uri = "https://hillsong-admin.okta.com/api/{0}/{1}{2}" -f $Version, $Endpoint, $QueryString #?filter=status eq "ACTIVE"'
+    Write-Verbose "GET [$uri]"
+  
 
-# make the API request using -FollowRelLink for automatic paging
-$response = Invoke-RestMethod -Headers $headers -Uri $uri -FollowRelLink
+    try {
+      $response = Invoke-RestMethod -Headers $headers -Uri $uri -FollowRelLink -Verbose:$false
+    }
+    catch [Microsoft.PowerShell.Commands.HttpResponseException] {
+      Write-Output "HTTP Error"
+    }
+    catch {
+      $_
+    }
+  }  
+  process {
+    # make the API request using -FollowRelLink for automatic paging
 
-# Unroll the pages
-$data = $response| Foreach-object {$_}
+    # Unroll the pages
+    $data = $response | Foreach-object { $_ }
 
-return $data
+    return $data
+  }
+  
 
 }
 # Choose one
 # $report | Export-Csv -NoTypeInformation ./okta_all_users_report_$((get-date -Format "yyyyMMdd")).csv -Force
 # $data | Export-Csv -NoTypeInformation ./okta_all_users_$((get-date -Format "yyyyMMdd")).csv -Force
+
+# Add public functions (only) here
+$functions = @(
+  "Get-Okta"
+)
+ 
+Export-ModuleMember -Function $functions
