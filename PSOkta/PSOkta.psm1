e2bfd291b86b8c1ec45940c926358c0e89ee41cf
adrw <adrw@192-168-1-9.tpgi.com.au>
2021-02-04 20:19:00 +1100

    added dynamic params

--- PSOkta/PSOkta.psm1
+++ PSOkta/PSOkta.psm1
@@ -4,59 +4,163 @@
 # Import Config
 $config = Import-PowerShellDataFile -Path ./Config.psd1
 
-function getHeaders {
-  
+function _getToken {
+  Read-Host -AsSecureString -Prompt "Please provide your Okta API Token" | Export-Clixml .token
+  Write-Verbose -Verbose "Encrypting token"
+}
+
+if (-not (Test-Path -Path $config.token_file)) {
+  Write-Warning -Verbose "Hold on, you don't have an API Token, standby";
+  getToken
+  if (-not (Test-Path -Path $config.token_file)) {
+    Write-Warning -Verbose "Still no Token file, get help, bailing."
+    Exit
+  }
+}
+
+function _getHeaders {
+  try {
+    $token = Import-Clixml $config.token_file | ConvertFrom-SecureString -AsPlainText
+  }
+  catch {
+    
+  }
   # Setup Auth Header
   return @{
-    Authorization  = "SSWS {0}" -f $config.token
+    Authorization  = "SSWS {0}" -f $token
     Accept         = "application/json"
     "Content-Type" = "application/json"
   }
 }
 
-$Global:headers = getHeaders
+$Global:headers = _getHeaders
 
 function Get-Okta {
   <#
  .Synopsis
-  Make requests to Okta API.
+  Gets Okta Users
+
+  .Example
+  # Get a single user and return the user profile.
+  RequestOkta -Version "v1" -Endpoint "users" -Q "username@hillsong.com" 
 
  .Example
-   # Query the user profile.
-   RequestOkta -Version "v1" -Endpoint "users" -QueryString "username@hillsong.com" 
+  # Get all users. Note: The -Version paramater defaults to "v1" and can be ommitted.
+  RequestOkta -Endpoint "users"
 
  .Example
-   # Get Okta ACTIVE users. Note: The -Version paramater defaults to "v1" and can be ommitted.
-   RequestOkta -Endpoint "users" -QueryString "status = 'ACTIVE'"
+  # Get Okta ACTIVE users. 
+  RequestOkta -Endpoint "users" -Filter "status = 'ACTIVE'"
+
+  .Example
+  # Get all users starting with "andrew" and return some useful objects
+  Get-Okta -Endpoint users -Q 'andrew' -Verbose | select id,status,@{n='name';e={$_.profile.login}},created,lastlogin  | ft
 
 #>
   [CmdletBinding()]
-  param (
+  Param (
     [Parameter()]
-    [String]
-    $Version = "v1",
-    [Parameter(Mandatory = $true)]
-    [String]
-    $Endpoint,
-    [Parameter()]
-    [String]
-    $Q,
-    [Parameter()]
-    [String]
-    $Filter,
-    [Parameter()]
-    [String]
-    $Search
+    [String]$Version = "v1",
+    [Parameter(Mandatory = $true, Position = 0)]
+    [ValidateSet("Users", "Groups", "Apps")]
+    [String]$Endpoint
   )
+
+  DynamicParam {
+    $ParamOptions = @{
+      Users  = @(
+        @{
+          ParamName = "Q"
+          ParamType = [String]
+        },
+        @{
+          ParamName = "All"
+          ParamType = [Switch]
+        },
+        @{
+          ParamName = "Active"
+          ParamType = [Switch]
+        },
+        @{
+          ParamName = "Locked"
+          ParamType = [Switch]
+        },
+        @{
+          ParamName = "PasswordExpired"
+          ParamType = [Switch]
+        },
+        @{
+          ParamName = "LastUpdated"
+          ParamType = [String]
+        },
+        @{
+          ParamName = "Department"
+          ParamType = [String]
+        }
+      )
+      Groups = @(
+        @{
+          ParamName = "LastMembershipUpdated"
+          ParamType = [String]
+        },
+        @{
+          ParamName = "Type"
+          ParamType = [String]
+        }
+      )
+      Apps   = @(
+        @{
+          ParamName = "Active"
+          ParamType = [Switch]
+        }
+      )
+    }
+
+    $RuntimeDefinedParameterDictionary = New-Object -TypeName System.Management.Automation.RuntimeDefinedParameterDictionary
+    function AddDynamicParams ($ParamName, $ParamType) {
+      $Collection = New-Object -TypeName System.Collections.ObjectModel.Collection[System.Attribute]
+      $ParameterAttribute = New-Object System.Management.Automation.ParameterAttribute
+      $ParameterAttribute.Mandatory = $false
+      $Collection.Add($ParameterAttribute)
+      $RuntimeDefinedParameter = New-Object -TypeName System.Management.Automation.RuntimeDefinedParameter($ParamName, $ParamType, $Collection)
+      $RuntimeDefinedParameterDictionary.Add($_.ParamName, $RuntimeDefinedParameter)
+    }
+
+    switch ($Endpoint) {
+      'Users' { 
+        $ParamOptions.Users.foreach{
+          AddDynamicParams -ParamName $_.ParamName -ParamType $_.ParamType
+        }
+        Break
+      }
+      'Groups' { 
+        $ParamOptions.Groups.foreach{
+          AddDynamicParams -ParamName $_.ParamName -ParamType $_.ParamType
+        }
+        Break
+      }
+      'Apps' { 
+        $ParamOptions.Apps.foreach{
+          AddDynamicParams -ParamName $_.ParamName -ParamType $_.ParamType
+        }
+        Break
+      }
+      Default {}
+    }
+
+    return $RuntimeDefinedParameterDictionary
+  }
 
   begin {
     # Handle the parameters
-    if ($Q) { $QueryString = -join ("?q=", $Q) }
-    if ($Filter) { $QueryString = -join ("?filter=", $Filter) }
-    if ($Search) { $QueryString = -join ("?search=", $Search) }
+    if ($PSBoundParameters.Q) { $QueryString = -join ("?q=", $PSBoundParameters.Q) }
+    if ($PSBoundParameters.All) { $null = $QueryString }
+    if ($PSBoundParameters.Active) { $QueryString = '?filter=status eq "ACTIVE"' }
+    if ($PSBoundParameters.Locked) { $QueryString = '?filter=status eq "LOCKED_OUT"' }
+    if ($PSBoundParameters.PasswordExpired) { $QueryString = '?filter=status eq "PASSWORD_EXPIRED"' }
 
     # API Resource Endpoint
-    $uri = -join(($config.base_uri),("/{0}/{1}{2}" -f $Version, $Endpoint, $QueryString)) #?filter=status eq "ACTIVE"'
+    $uri = -join (($config.base_uri), ("/{0}/{1}{2}" -f $Version, $Endpoint.ToLower(), $QueryString)) #?filter=status eq "ACTIVE"'
     Write-Verbose "GET [$uri]"
 
     try {
